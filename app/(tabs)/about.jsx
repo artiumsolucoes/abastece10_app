74aae03dde9e85f7aebf52f1d1c3f70d95de46da
JoaoVitorHz <vitorjoao39207@gmail.com>
2025-06-03 10:29:20 -0300

    tailwind

---
+++ app/(tabs)/about.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Screen(){
+    return(
+        <View>
+            <Text className="text-red-200 font-bold">ABOUT</Text>
+        </View>
+    )
+}
---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import '../../global.css'
+export default function Screen(){
+    return(
+        <View >
+            <Text className="text-red-200 font-bold">Index Teste 21</Text>
+        </View>
+    )
+}
